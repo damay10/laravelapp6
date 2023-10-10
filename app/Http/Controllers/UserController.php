22a867066663368e9768ecbc8ce77f32e77fe554
damay10 <angelinamahesti1@gmail.com>
2023-10-10 20:14:53 +0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use UserController extend Controller {
+    public function index() {
+        return "Ini adalah controller";
+    }
+}
---
+++ resources/views/about.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="about" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="">
+</head>
+<style>
+    body, h1, h2, p {
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 20px;
+}
+
+nav {
+    background-color: #444;
+}
+
+nav ul {
+    list-style-type:none;
+    padding: 0;
+    margin: 0;
+    text-align: center;
+}
+
+nav li {
+    display: inline;
+    margin-right: 20px;
+}
+
+nav a {
+    text-decorate: none;
+    color: #fff;
+}
+
+main {
+    padding: 20px;
+}
+
+footer {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+}
+</style>
+<body>
+    <header>
+        <h1>About Us</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+    </nav>
+    <main>
+        <section>
+            <h2>Our Member</h2>
+            <ul>
+                <li>Damayanti Wahyu Mahesti</i>
+            </ul>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2023 Flower Bouqet</p>
+    </footer>  
+</body>
+</html>
